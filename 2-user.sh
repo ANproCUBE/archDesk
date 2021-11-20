12b3c7804d70fa0d38e4bb0eff3d6081df5b4701
ANproCUBE <71607868+ANproCUBE@users.noreply.github.com>
2021-11-20 18:41:16 +0100

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -5,22 +5,38 @@ git clone "https://aur.archlinux.org/yay.git"
 cd ${HOME}/yay
 makepkg -si --noconfirm
 cd ~
+touch "$HOME/.cache/zshhistory"
+git clone "https://github.com/ChrisTitusTech/zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
 PKGS=(
+'autojump'
 'awesome-terminal-fonts'
+'brave-bin' # Brave Browser
 'dxvk-bin' # DXVK DirectX to Vulcan
+'github-desktop-bin' # Github Desktop sync
 'lightly-git'
+'lightlyshaders-git'
+'mangohud' # Gaming FPS Counter
 'mangohud-common'
 'nerd-fonts-fira-code'
 'nordic-darker-standard-buttons-theme'
 'nordic-darker-theme'
 'nordic-kde-git'
 'nordic-theme'
+'noto-fonts-emoji'
 'papirus-icon-theme'
+'plasma-pa'
+'ocs-url' # install packages from websites
+'sddm-nordic-theme-git'
+'snapper-gui-git'
 'ttf-droid'
 'ttf-hack'
 'ttf-meslo' # Nerdfont package
 'ttf-roboto'
+'zoom' # video conferences
+'snap-pac'
 )
 
 for PKG in "${PKGS[@]}"; do
@@ -29,10 +45,10 @@ done
 
 export PATH=$PATH:~/.local/bin
 cp -r $HOME/ArchDesktop/dotfiles/* $HOME/.config/
-#pip install konsave
-#konsave -i $HOME/ArchDesktop/kde.knsv
-#sleep 1
-#konsave -a kde
+pip install konsave
+konsave -i $HOME/ArchDesktop/kde.knsv
+sleep 1
+konsave -a kde
 
 echo -e "\nDone!\n"
 exit
